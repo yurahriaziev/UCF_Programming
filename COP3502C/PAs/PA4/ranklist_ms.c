8aeb4be7786b76fc9f44da22027b8d8ff26b767c
yurahriaziev <yura.hriaziev@gmail.com>
2026-03-23 22:27:33 -0400

    fix

--- COP3502C/PAs/PA4/ranklist_ms.c
+++ COP3502C/PAs/PA4/ranklist_ms.c
@@ -1,7 +1,7 @@
 /*              COP 3502C PA1
 This program is written by: Yurii Hriaziev */
 
-// this program implements the assignment using Quick Sort
+// this program implements the assignment using Merge Sort
 
 // given code
 #include <stdio.h>
