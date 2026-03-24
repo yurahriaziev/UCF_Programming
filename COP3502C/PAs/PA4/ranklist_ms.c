3b7c7f7459e4526f9c5dc7b077b109332aaf6948
yurahriaziev <yura.hriaziev@gmail.com>
2026-03-23 22:50:05 -0400

    added compareTo function

--- COP3502C/PAs/PA4/ranklist_ms.c
+++ COP3502C/PAs/PA4/ranklist_ms.c
@@ -33,3 +33,19 @@ void insertionSort(Cat **list, int low, int high, int key);
 Cat **readCats(int n);
 void printRanks(Cat **list, int n, int key);
 void freeCats(Cat **list, int n);
+
+// compareTo: function that takes in two pointers ptrC1, ptrC2 to Cat structs
+// - returns -1 if ptrC1 cat comes before ptrC2 cat
+// - returns 0 if both cats are identical
+// - returns 1 if ptrC2 cat comes before ptrC1 cat
+// - if cat scores are tied, rank names alphabetically
+int compareTo(Cat *ptrC1, Cat *ptrC2, int key) {
+    if (ptrC1->scores[key] > ptrC2->scores[key]) {
+        return -1;
+    }
+    if (ptrC1->scores[key] < ptrC2->scores[key]) {
+        return 1;
+    }
+
+    return strcmp(ptrC1->name, ptrC2->name);
+}
