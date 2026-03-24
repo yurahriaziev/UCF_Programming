0b277d377e7b271ad95bfc5e3dde0c722e65bb2b
yurahriaziev <yura.hriaziev@gmail.com>
2026-03-24 12:38:01 -0400

    started quick sort version of assignment

--- COP3502C/PAs/PA4/ranklist_qs.c
+++ COP3502C/PAs/PA4/ranklist_qs.c
@@ -26,5 +26,52 @@ typedef struct Cat {
 
 // function prototypes
 int compareTo(Cat *ptrC1, Cat *ptrC2, int key);
+void insertionSort(Cat **list, int low, int high, int key);
+Cat **readCats(int n);
+void printRanks(Cat **list, int n, int key);
+void freeCats(Cat **list, int n);
 void quickSort(Cat **list, int n, int key);
 void quickSortRec(Cat **list, int low, int high, int key);
+void swap(Cat *catA, Cat *catB);
+int partition(Cat **list, int low, int high, int key);
+
+// printRanks: function that will print each cat in the given print format
+void printRanks(Cat **list, int n, int key) {
+    if (list == NULL) {
+        return;
+    }
+
+    printf("%s Ranklist\n", TRAITS[key]);
+    for (int i=0; i<n; i++) {
+        printf("%d. %-15s %d\n", i+1, list[i]->name, list[i]->scores[key]);
+    }
+}
+
+// freeCats: function that will free all cat structs and the cat list too
+void freeCats(Cat **list, int n) {
+    if (list == NULL) {
+        return;
+    }
+
+    for (int i=0; i<n; i++) {
+        free(list[i]->name);
+        free(list[i]);
+    }
+
+    free(list);
+}
+
+int main(void) {
+    int n, key;
+
+    scanf("%d", &n);
+    Cat **list = readCats(n);
+    scanf("%d", key);
+
+
+
+    printRanks(list, n, key);
+    freeCats(list, n);
+
+    return 0;
+}
