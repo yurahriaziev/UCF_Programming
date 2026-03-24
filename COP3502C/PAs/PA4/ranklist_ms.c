8fdf59e0ada2ce7f9a9703ee9431475651b6c20a
yurahriaziev <yura.hriaziev@gmail.com>
2026-03-23 21:30:11 -0400

    PA4 two files created

---
+++ COP3502C/PAs/PA4/ranklist_ms.c
@@ -0,0 +1,4 @@
+/*              COP 3502C PA1
+This program is written by: Yurii Hriaziev */
+
+// this program implements the assignment using Merge Sort
---
+++ COP3502C/PAs/PA4/ranklist_qs.c
@@ -0,0 +1,5 @@
+/*              COP 3502C PA1
+This program is written by: Yurii Hriaziev */
+
+// this program implements the assignment using Quick Sort
+
