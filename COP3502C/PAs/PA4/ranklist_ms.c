2278f07eeca1bbd58c023d0b52b106da69bc9496
yurahriaziev <yura.hriaziev@gmail.com>
2026-03-23 22:24:21 -0400

    given code added

--- COP3502C/PAs/PA4/ranklist_ms.c
+++ COP3502C/PAs/PA4/ranklist_ms.c
@@ -1,4 +1,30 @@
 /*              COP 3502C PA1
 This program is written by: Yurii Hriaziev */
 
-// this program implements the assignment using Merge Sort
+// this program implements the assignment using Quick Sort
+
+// given code
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 12
+#define NUMTRAITS 7
+#define TOTAL 6
+#define BASECASESIZE 30
+
+const char TRAITS[NUMTRAITS][MAXSIZE+1] = 
+{
+    "Cuteness", "Fluffiness", "Agility",
+    "Friendliness", "Intelligence", "Laziness", "Total"
+};
+
+typedef struct Cat {
+    char *name;
+    int scores[NUMTRAITS];
+} Cat;
+
+// function prototypes
+int compareTo(Cat *ptrC1, Cat *ptrC2, int key);
+void mergeSort(Cat **list, int n, int key);
+void mergeSortRec(Cat **list, int low, int high, int key);
--- COP3502C/PAs/PA4/ranklist_qs.c
+++ COP3502C/PAs/PA4/ranklist_qs.c
@@ -3,3 +3,28 @@ This program is written by: Yurii Hriaziev */
 
 // this program implements the assignment using Quick Sort
 
+// given code
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 12
+#define NUMTRAITS 7
+#define TOTAL 6
+#define BASECASESIZE 30
+
+const char TRAITS[NUMTRAITS][MAXSIZE+1] = 
+{
+    "Cuteness", "Fluffiness", "Agility",
+    "Friendliness", "Intelligence", "Laziness", "Total"
+};
+
+typedef struct Cat {
+    char *name;
+    int scores[NUMTRAITS];
+} Cat;
+
+// function prototypes
+int compareTo(Cat *ptrC1, Cat *ptrC2, int key);
+void quickSort(Cat **list, int n, int key);
+void quickSortRec(Cat **list, int low, int high, int key);
