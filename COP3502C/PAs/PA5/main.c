5c47b5e9542c60ec527585763c2349493c6e27a5
yurahriaziev <yura.hriaziev@gmail.com>
2026-04-13 20:54:50 -0400

    started PA5, added create cat and create bst node functions

---
+++ COP3502C/PAs/PA5/main.c
@@ -0,0 +1,66 @@
+/*              COP 3502C PA 5
+This program is written by: Yurii Hriaziev */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// given code
+#define MAX_NAME 25 // max name of a cat
+#define NUM_TRAITS 5 // number of total traits a cat has
+typedef struct Cat {
+    char *name; //dynamically allocate space for a cat’s name
+    char *breed; //dynamically allocate space for a cat’s breed
+    int charm; //unique charm score for each cat
+    int traits[NUM_TRAITS]; //scores corresponding to each index in TRAIT_NAMES
+} Cat;
+
+typedef struct BST_Node {
+    Cat *cat;
+    struct BST_Node *left;
+    struct BST_Node *right;
+    int subtree_size;
+} BST_Node;
+
+const char *TRAIT_NAMES[NUM_TRAITS] = {"friendly", "grumpy", "playful", "lazy", "curious"};
+// end given code
+
+// function prototypes
+Cat *createCat();
+BST_Node *createBSTNode(Cat *cat);
+
+// createCat: function that will create a new cat with given name, breed, charm and traits
+Cat *createCat() {
+    Cat *newCat = malloc(sizeof(Cat));
+
+    char tempName[MAX_NAME+1];
+    char tempBreed[MAX_NAME+1];
+    int charm;
+    
+    scanf("%s %s %d", tempName, tempBreed, &charm);
+
+    newCat->name = malloc((strlen(tempName) + 1) * sizeof(char));
+    strcpy(newCat->name, tempName);
+    
+    newCat->breed = malloc((strlen(tempBreed) + 1) * sizeof(char));
+    strcpy(newCat->breed, tempBreed);
+
+    newCat->charm = charm;
+
+    for (int i=0; i<NUM_TRAITS; i++) {
+        scanf("%d", &newCat->traits[i]);
+    }
+
+    return newCat;
+}
+
+BST_Node *createBSTNode(Cat *cat) {
+    BST_Node *newNode = malloc(sizeof(BST_Node));
+
+    newNode->cat = cat;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->subtree_size = 1;
+
+    return newNode;
+}
