c822b036e0a3e38da82263cd1d386f87c4ed7a74
yurahriaziev <yura.hriaziev@gmail.com>
2026-04-13 21:52:17 -0400

    insertion works and tested

--- COP3502C/PAs/PA5/main.c
+++ COP3502C/PAs/PA5/main.c
@@ -30,6 +30,8 @@ Cat *createCat();
 BST_Node *createBSTNode(Cat *cat);
 BST_Node *findNode(BST_Node *root, char *name);
 int countTraitsForCat(Cat *cat);
+BST_Node *insert(BST_Node *root, BST_Node *newNode, int depth);
+void freeCat(Cat *cat);
 
 // createCat: function that will create a new cat with given name, breed, charm and traits
 Cat *createCat() {
@@ -68,6 +70,7 @@ BST_Node *createBSTNode(Cat *cat) {
     return newNode;
 }
 
+// findNode: function that will find a return the BST node in the BST with a given key which is the name of the cat
 BST_Node *findNode(BST_Node *root, char *name) {
     if (root != NULL) {
         if (strcmp(root->cat->name, name) == 0) {
@@ -84,6 +87,7 @@ BST_Node *findNode(BST_Node *root, char *name) {
     return NULL;
 }
 
+// countTraitsForCat: helper function that will return the count of traits a cat has if a duplicate cat is being inserted
 int countTraitsForCat(Cat *cat) {
     int count = 0;
 
@@ -100,6 +104,69 @@ int countTraitsForCat(Cat *cat) {
     return count;
 }
 
+// insert: 
+BST_Node *insert(BST_Node *root, BST_Node *newNode, int depth) {
+    if (root == NULL) {
+        printf("Insert: %d\n", depth);
+        return newNode;
+    }
+
+    if (strcmp(newNode->cat->name, root->cat->name) == 0) {
+        if (countTraitsForCat(newNode->cat) > countTraitsForCat(root->cat)) {
+            freeCat(root->cat);
+
+            root->cat = newNode->cat;
+            free(newNode);
+
+            printf("Replaced\n");
+        } else {
+            free(newNode->cat->name);
+            free(newNode->cat->breed);
+            free(newNode->cat);
+            free(newNode);
+        }
+        return root;
+    }
+
+    if (strcmp(newNode->cat->name, root->cat->name) < 0) {
+        root->left = insert(root->left, newNode, depth+1);
+    } else {
+        root->right = insert(root->right, newNode, depth+1);
+    }
+
+    root->subtree_size = 1;
+    if (root->left != NULL) {
+        root->subtree_size += root->left->subtree_size;
+    }
+    if (root->right != NULL) {
+        root->subtree_size += root->right->subtree_size;
+    }
+
+    return root;
+}
+
+void freeCat(Cat *cat) {
+    free(cat->name);
+    free(cat->breed);
+    free(cat);
+}
+
 int main() {
+    BST_Node *root = NULL;
+    int num_ops;
+    int q;
+
+    scanf("%d", &num_ops);
+
+    for (int i=0; i<num_ops; i++) {
+        scanf("%d", &q);
+
+        if (q == 1) {
+            Cat *newCat = createCat();
+            BST_Node *newNode = createBSTNode(newCat);
+            root = insert(root, newNode, 0);
+        }
+    }
+
     return 0;
 }
